3c4fee80c5649b7782d3909491268ee218dd9f6c
dauster <wall-e@dauster.local>
2020-11-08 11:47:06 -0300

    add comment and variable session

---
+++ fundamentals/comment.js
@@ -0,0 +1,14 @@
+// comment with on line
+console.log("Line 1");
+
+/*
+comment with 
+multi lines 
+*/
+console.log("Line 2");
+
+/*
+* comment with 
+* bullet points
+*/
+console.log("Line 3");
---
+++ fundamentals/organization.js
@@ -0,0 +1,10 @@
+// sentence
+console.log(
+    "Code Sentence");
+// blocks
+{
+    {
+        console.log("Hello");
+        console.log("World!!");
+    }
+}
